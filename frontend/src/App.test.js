d680daee7123c36ceb3ab2cec6f3286d04247dd4
Lindokuhle777 <92369482+Lindokuhle777@users.noreply.github.com>
2022-05-08 18:30:50 +0200

    codecov update

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import Dummy from './dummy'
+import React from "react";
 
 test('checkExplore', () => {
   const{getByTestId} = render(<Dummy/>);
